91abea42ee160147bbf29260fc5117bf0492ff0b
darrenhoon <darrenhoon.ad@gmail.com>
2021-05-17 16:03:32 +0800

    added admin routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
+import { AdminBoardComponent } from "./admin/admin-board/admin-board.component";
 import { LoginComponent } from "./auth/login/login.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { PostBoardComponent } from "./posts/posts-board/posts-board.component";
@@ -8,9 +9,10 @@ import { PostCreateComponent } from "./posts/posts-create/posts-create.component
 
 const routes: Routes = [
   { path: 'dashboard', component: PostBoardComponent },
+  { path: 'admin', component: AdminBoardComponent },
   { path: 'create', component: PostCreateComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
 
 ];
 @NgModule({
