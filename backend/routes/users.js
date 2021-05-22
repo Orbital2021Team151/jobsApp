c1070ab6168c64d3b9eea1be74add5e31c2024e4
darrenhoon <darrenhoon.ad@gmail.com>
2021-05-22 08:36:47 +0800

    fixed login attempt reset issue when wrong info is provided

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,14 +9,17 @@ const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
 
-  bcrypt.hash(req.body.password, 10).then(hash => { //TODO: might have to hash role here too.
+  bcrypt.hash(req.body.password, 10)
+  .then(passwordHash => { //TODO: might have to hash role here too.
+
     const user = new User({
       email: req.body.email,
-      password: hash,
+      password: passwordHash,
       role: req.body.role,
       orgName: req.body.orgName,
       uen: req.body.uen,
     });
+
     user
       .save()
       .then(result => {
@@ -31,6 +34,8 @@ router.post("/signup", (req, res, next) => {
           error: err
         });
       });
+
+
   });
 });
 
@@ -42,11 +47,16 @@ router.post("/login", (req, res, next) => {
     email: req.body.email,
     role: req.body.role,
   })
+
   .then(user => {
     if (!user) {
+
+      /*
       return res.status(401).json({
         message: "Authentication failed"
-      })
+      });
+      */
+      throw new Error("Authentication Failed 1");
     }
     fetchedUser = user;
     return bcrypt.compare(req.body.password, user.password); // , user.role);
@@ -54,10 +64,14 @@ router.post("/login", (req, res, next) => {
 
   .then(result => {
     if (!result) {
+      /*
       return res.status(401).json({
         message: "Authentication failed"
-      })
+      });
+      */
+      throw new Error("Authentication Failed 1");
     }
+
     const token = jwt.sign({
       email: fetchedUser.email,
       userId: fetchedUser._id,
@@ -66,13 +80,6 @@ router.post("/login", (req, res, next) => {
     'secret_token_that_no_one_will_find_out_about',
     { expiresIn: "1h" }
     );
-
-    console.log("From Login, below data are retrieved");
-    console.log(fetchedUser);
-    console.log(fetchedUser.email);
-    console.log(fetchedUser.orgName);
-    console.log(fetchedUser.uen);
-    console.log(fetchedUser.password);
 
     res.status(200).json({
       token: token,
@@ -83,12 +90,11 @@ router.post("/login", (req, res, next) => {
     })
   })
 
-
   .catch(err => {
     return res.status(401).json({
       message: "Authentication failed"
     })
-  })
+  });
 });
 
 module.exports = router;
