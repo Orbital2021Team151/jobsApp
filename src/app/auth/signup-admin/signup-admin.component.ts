37b0bbc84940d009ba7255cac2f57bc16561c06f
darrenhoon <darrenhoon.ad@gmail.com>
2021-07-22 14:56:13 +0800

    fixed signup admin to be reactive

--- src/app/auth/signup-admin/signup-admin.component.ts
+++ src/app/auth/signup-admin/signup-admin.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnDestroy, OnInit, ViewEncapsulation } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { NgForm, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { MatDialog } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import { Subscription } from "rxjs";
+import { WrongStudentEmailDialog } from "src/app/dialogs/wrong-student-email-dialog/wrong-student-email-dialog.component";
 import { AuthService } from "../auth.service";
+import { SignupAlertComponent } from "../signup-alert/signup-alert.component";
 
 @Component({
   templateUrl: './signup-admin.component.html',
@@ -12,11 +16,28 @@ export class SignupAdminComponent implements OnInit, OnDestroy {
   isLoading = false;
   roles: any = ["Admin", "External Organisation", "Student Organisation", "Student / NUS Alumni"];
   hidePassword = true;
-  signedUp: boolean;
   //private authStatusObject;
   private authStatusSub: Subscription;
 
-  constructor(public authService: AuthService) {}
+  /* FormGroup version */
+  public form: FormGroup;
+  public roleControl = new FormControl(null);
+  public orgNameControl = new FormControl(null);
+  public emailControl = new FormControl(null);
+  public uenControl = new FormControl(null);
+  public passwordControl = new FormControl(null);
+
+  constructor(public authService: AuthService, private _snackBar: MatSnackBar, public dialog: MatDialog) {
+    this.form = new FormGroup({
+      //hideRequired: this.hideRequiredControl,
+      //floatLabel: this.floatLabelControl,
+      role: this.roleControl,
+      orgName: this.orgNameControl,
+      email: this.emailControl,
+      uen: this.uenControl,
+      password: this.passwordControl,
+    });
+  }
 
   ngOnInit() {
     //this.authStatusObject = this.authService.getAuthStatusObject();
@@ -25,24 +46,51 @@ export class SignupAdminComponent implements OnInit, OnDestroy {
       authObject => {
         if (!authObject.auth) {
           this.isLoading = false;
-          this.signedUp = false;
         }
       }
     );
   }
 
-  onSignup(form: NgForm) {
-    if (form.invalid) {
+
+  onSignupReactive() {
+
+    console.log("Reactive form is:");
+    console.log(this.form);
+
+    if (this.form.value.role === "Student / NUS Alumni") {
+      if (this.form.value.email.indexOf("@u.nus.edu") == -1) {
+        this.dialog.open(WrongStudentEmailDialog);
+        return;
+      }
+    }
+
+    if (this.form.invalid) {
+      //console.log("Reactive Form is invalid! It is:");
+      //console.log(this.form);
       return;
     }
 
-    this.authService.createUserAdmin(form.value.email, form.value.password, form.value.role, form.value.orgName, form.value.uen);
+    this.authService.createUser(
+      this.form.value.email,
+      this.form.value.password,
+      this.form.value.role,
+      this.form.value.orgName,
+      this.form.value.uen
+    );
 
-    //IDK IF THIS FIXES IT BUT I THINK IT IS WORKING?
     this.authService.getSignupListener().subscribe(result => {
-      this.signedUp = result;
+      this.openSignupSuccessSnackBar();
+      this.form.reset();
+      Object.keys(this.form.controls).forEach(key =>{
+        this.form.controls[key].setErrors(null)
+     });
     });
+  }
 
+  openSignupSuccessSnackBar() {
+    this._snackBar.openFromComponent(SignupAlertComponent, {
+      duration: 3 * 1000,
+    });
   }
 
   ngOnDestroy() {
---
+++ src/app/auth/signup-alert/signup-alert.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'signup-alert-component',
+  templateUrl: 'signup-alert.component.html',
+  styleUrls: ['signup-alert.component.css'],
+})
+export class SignupAlertComponent {}
