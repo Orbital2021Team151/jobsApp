357d7dbbaac8b29e47383bd2f8734bcbc31a2acd
darrenhoon <darrenhoon.ad@gmail.com>
2021-07-30 13:23:10 +0800

    dist for heroku built

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/',  //https://ccsgp-app.herokuapp.com/ or http://ccsgp-portal.us-east-2.elasticbeanstalk.com/ or http://localhost:3000/
+  apiUrl: 'https://ccsgp-app.herokuapp.com/',  //https://ccsgp-app.herokuapp.com/ or http://ccsgp-portal.us-east-2.elasticbeanstalk.com/ or http://localhost:3000/
 };
