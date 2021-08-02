aee871c344e17ddf2877af42b9ec8ce13e82bf3e
darrenhoon <darrenhoon.ad@gmail.com>
2021-08-02 21:43:17 +0800

    changed the pending approval

--- src/app/admin/approve-posts-page/approve-posts-page.component.ts
+++ src/app/admin/approve-posts-page/approve-posts-page.component.ts
@@ -1,6 +1,15 @@
-import { Component, OnDestroy, OnInit } from '@angular/core'
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormGroup, FormControl, NgForm } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
@@ -9,53 +18,69 @@ import { PostsService } from 'src/app/posts/post.service';
 import { ChangePasswordAlertComponent } from '../snackbars/change-password-snackbar/change-password-snackbar.component';
 
 @Component({
-    selector: 'approve-posts',
-    templateUrl: './approve-posts-page.component.html',
-    styleUrls: ['approve-posts-page.component.css']
+  selector: 'approve-posts',
+  templateUrl: './approve-posts-page.component.html',
+  styleUrls: ['approve-posts-page.component.scss'],
 })
-export class ApprovePostsPageComponent implements OnInit, OnDestroy {
-
-    list = '1'.repeat(100).split('').map((_, i) => i); //??????
-
+export class ApprovePostsPageComponent
+  implements OnInit, OnDestroy, AfterViewInit
+{
   posts: Post[] = [];
 
   private postSub: Subscription;
   private authStatusSub: Subscription;
   private authStatusObject;
 
-  private postsNumber: number;
+  public postsNumber: number;
   public hasRequest: Boolean;
 
+  /* Filter Table */
+  displayedColumns: string[] = ['POC', 'email', 'phoneNumber', 'menu'];
+  dataSource: MatTableDataSource<Post> = new MatTableDataSource<Post>();
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   /* Reject FormGroup */
   public rejectForm: FormGroup;
   public rejectReasonControl = new FormControl(null);
 
   //navbar stuff
-  active: string = "postsPendingApproval";
+  active: string = 'postsPendingApproval';
 
-  constructor(public postsService: PostsService, private modalService: NgbModal, public authService: AuthService, private _snackBar: MatSnackBar) {
+  constructor(
+    public postsService: PostsService,
+    private modalService: NgbModal,
+    public authService: AuthService,
+    private _snackBar: MatSnackBar
+  ) {
     this.rejectForm = new FormGroup({
       reason: this.rejectReasonControl,
     });
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
     this.postsService.getPosts();
     this.authStatusObject = this.authService.getAuthStatusObject();
 
-    this.postSub = this.postsService.getPostsUpdatedListener()
+    this.postSub = this.postsService
+      .getPostsUpdatedListener()
       .subscribe((posts: Post[]) => {
-
-        this.posts = posts.filter(post => !post.removed);
-        this.posts = this.posts.filter(post => !post.approved);
+        this.posts = posts.filter((post) => !post.removed);
+        this.posts = this.posts.filter((post) => !post.approved);
 
         if (this.posts.length > 0) {
           this.hasRequest = true;
-          this.postsNumber = this.posts.filter(post => !post.approved).length;
+          this.postsNumber = this.posts.filter((post) => !post.approved).length;
         } else {
           this.hasRequest = false;
         }
+
+        this.dataSource = new MatTableDataSource(this.posts);
+
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       });
   }
 
@@ -88,7 +113,6 @@ export class ApprovePostsPageComponent implements OnInit, OnDestroy {
   }
 
   onPublish(postId: string, publishContent) {
-
     this.postsService.publishPost(postId);
 
     this.modalService.open(publishContent, { scrollable: true });
@@ -108,6 +132,17 @@ export class ApprovePostsPageComponent implements OnInit, OnDestroy {
     this.modalService.open(reportedContent, { size: 'lg' });
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    //console.log("Apply Filter function called!");
+    //console.log(filterValue);
+    //console.log(this.dataSource.filter);
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 
   ngOnDestroy() {
     this.postSub.unsubscribe();
